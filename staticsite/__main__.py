aa91fe2bdd11c4bbedac1b551805c2e60bf14e56
arjoonn sharma <arjoonn.94@gmail.com>
2020-05-03 08:41:44 +0530

    switch to poetry

--- staticsite/__main__.py
+++ staticsite/__main__.py
@@ -6,25 +6,32 @@ from .builder import build
 from .watcher import watch
 
 parser = argparse.ArgumentParser(
+    prog="python -m staticsite",
     description=(
-        "Helper functions to build a static site. Folder structure of"
+        "A super simple static site generator using Jinja2. The folder structure of "
         "the source is preserved.Files starting with `.` are ignored."
-    )
+    ),
 )
+parser.add_argument("--src", default="src")
+parser.add_argument("--target", default="www")
 parser.add_argument(
     "cmd",
     default="build",
     choices=["build", "watch"],
-    help="What action do you want to take?",
+    help="What action do you want to take? "
+    "Build will simply compile the static site into the target directory "
+    "while watch will re-build the site any time the source changes and reload the browser when it does.",
 )
-parser.add_argument("--src", default="src")
-parser.add_argument("--target", default="docs")
-parser.add_argument("--config", default="staticsite.yaml")
+parser.add_argument(
+    "--config",
+    default="staticsite.yaml",
+    help="The variables in this file are made available to the templates."
+    " You can use this to set things like the base url for your site.",
+)
 
 args = parser.parse_args()
 with open(args.config, "r") as fl:
     config = yaml.load(fl, Loader=yaml.FullLoader)
-    print(config)
 if args.cmd == "build":
     build(src=args.src, target=args.target, config=config)
 elif args.cmd == "watch":
